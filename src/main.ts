a819d46cb1acf5f542cead0179414a27626b9cfb
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-10-31 23:16:52 +0200

    tried to add anime in scroll

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// plugins
+// import { MotionPlugin } from '@vueuse/motion'
+// .use(MotionPlugin)
+
+
 createApp(App).mount('#app')
