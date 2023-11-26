ee240b9dceb2ba47b702f92504ee79f672e66730
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-11-26 22:47:42 +0200

    added splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
+import { trigger, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ CommonModule, RouterOutlet, SplashComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave', [
+        animate('1s', style({ opacity: 0 })) // Animation for fading out
+      ])
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'monzeromer';
+
+  isLoading: boolean = true;
+  splashVisible: boolean = false;
+
+
+  ngOnInit() {
+    // Simulate an asynchronous operation (e.g., API call or initialization)
+    setTimeout(() => {
+      this.splashVisible = true;
+      this.isLoading = false;
+    }, 5000); // Adjust the duration as needed
+  }
+
+
 }
---
+++ src/app/splash/splash.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-splash',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './splash.component.html',
+  styleUrl: './splash.component.css'
+})
+export class SplashComponent {
+
+
+}
