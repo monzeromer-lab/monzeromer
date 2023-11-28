a7e59de073acb0d897d8f45b00cd30fce6e7100f
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-11-28 07:48:24 +0200

    I did something but idk

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-splash',
   standalone: true,
   imports: [CommonModule],
+  animations: [
+    trigger('typingAnimation', [
+      state('start', style({ width: '0' })),
+      state('end', style({ width: '100%' })),
+      transition('start => end', animate('1s steps(20, end)'))
+    ])
+  ],
   templateUrl: './splash.component.html',
   styleUrl: './splash.component.css'
 })
 export class SplashComponent {
 
+  animationState: string = 'start';
+
+  ngOnInit() {
+    // Trigger the animation after a short delay (adjust as needed)
+    setTimeout(() => {
+      this.animationState = 'end';
+    }, 500);
+  }
+
+  
 
 }
