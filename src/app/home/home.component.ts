6ded9b77f053bdcbf778b5d12ef0b97e3096086c
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-11-30 18:18:39 +0200

    image opt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { MobileNavComponent } from '../components/mobile-nav/mobile-nav.component';
 import { RouterModule } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, MobileNavComponent, RouterModule],
+  imports: [CommonModule, NavbarComponent, MobileNavComponent, RouterModule, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
